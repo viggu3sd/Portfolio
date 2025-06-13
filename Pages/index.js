8504656e28bc77217a6b503224456763a91eedf4
Sai Vignesh <b22es023@iitj.ac.in>
2025-06-13 23:36:37 +0530

    Updated Next js app

---
+++ Pages/index.js
@@ -0,0 +1,27 @@
+// pages/index.js
+import Layout from '../components/Layout';
+
+export default function Home() {
+  return (
+    <Layout>
+      <section id="about" className="container mx-auto py-16">
+        {/* About Me content */}
+      </section>
+      <section id="interests" className="container mx-auto py-16">
+        {/* Interests content */}
+      </section>
+      <section id="skills" className="container mx-auto py-16">
+        {/* Skills content */}
+      </section>
+      <section id="experiences" className="container mx-auto py-16">
+        {/* Experiences content */}
+      </section>
+      <section id="projects" className="container mx-auto py-16">
+        {/* Projects content */}
+      </section>
+      <section id="contact" className="container mx-auto py-16">
+        {/* Contact form content */}
+      </section>
+    </Layout>
+  );
+}
