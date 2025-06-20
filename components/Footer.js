6bc9005f2cdf11ca4f99b173c4bab8a546b45b6d
Sai Vignesh <146276824+viggu3sd@users.noreply.github.com>
2025-06-20 23:02:58 +0530

    Updated Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,4 +1,3 @@
-
 const Footer = () => (
   <footer className="bg-gray-100 text-center py-4 mt-10">
     <p className="text-gray-600">&copy; {new Date().getFullYear()} YourName. All rights reserved.</p>
